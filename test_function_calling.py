e6a83c04c1e5d1a7079652985333e7c03225134b
TakanariShimbo <takanari.shimbo@gmail.com>
2024-11-20 09:12:43 +0900

    update

--- test_function_calling.py
+++ test_function_calling.py
@@ -4,6 +4,7 @@ from typing import Any
 
 from dotenv import load_dotenv
 from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageToolCall, ChatCompletionMessage
 
 
 def initialize_openai_client() -> OpenAI:
@@ -13,9 +14,44 @@ def initialize_openai_client() -> OpenAI:
     return OpenAI(api_key=api_key)
 
 
-def add_numbers(num1: float, num2: float) -> float:
+def _add_numbers(num1: float, num2: float) -> float:
     """2つの数を足し算して結果を返す"""
     return num1 + num2
+
+
+def add_numbers(tool_call: ChatCompletionMessageToolCall) -> dict[str, Any]:
+    """2つの数を足し算して結果を返す"""
+    arguments = json.loads(s=tool_call.function.arguments)
+    num1 = arguments.get("num1")
+    num2 = arguments.get("num2")
+
+    result = _add_numbers(num1=num1, num2=num2)
+
+    return {
+        "role": "tool",
+        "content": json.dumps(obj={"num1": num1, "num2": num2, "result": result}),
+        "tool_call_id": tool_call.id,
+    }
+
+
+def _multiply_numbers(num1: float, num2: float) -> float:
+    """2つの数を掛け算して結果を返す"""
+    return num1 * num2
+
+
+def multiply_numbers(tool_call: ChatCompletionMessageToolCall) -> dict[str, Any]:
+    """2つの数を掛け算して結果を返す"""
+    arguments = json.loads(s=tool_call.function.arguments)
+    num1 = arguments.get("num1")
+    num2 = arguments.get("num2")
+
+    result = _multiply_numbers(num1=num1, num2=num2)
+
+    return {
+        "role": "tool",
+        "content": json.dumps(obj={"num1": num1, "num2": num2, "result": result}),
+        "tool_call_id": tool_call.id,
+    }
 
 
 def get_tools_definition() -> list[dict[str, Any]]:
@@ -36,7 +72,23 @@ def get_tools_definition() -> list[dict[str, Any]]:
                     "additionalProperties": False,
                 },
             },
-        }
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "multiply_numbers",
+                "description": "2つの数を掛けます。ユーザーが2つの数の積を求めたときにこの関数を使用してください。",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "num1": {"type": "number", "description": "最初に掛ける数。"},
+                        "num2": {"type": "number", "description": "次に掛ける数。"},
+                    },
+                    "required": ["num1", "num2"],
+                    "additionalProperties": False,
+                },
+            },
+        },
     ]
 
 
@@ -48,34 +100,41 @@ def get_system_message() -> dict[str, Any]:
     }
 
 
-def first_query(client: OpenAI, model: str, messages: list[dict[str, Any]], tools: list[dict[str, Any]]) -> tuple[bool, list[dict[str, Any]]]:
+def first_query(
+    client: OpenAI,
+    model: str,
+    messages: list[dict[str, Any]],
+    tools: list[dict[str, Any]],
+) -> tuple[str | None, list[dict[str, Any]]]:
     """ツール呼び出しを含むメッセージを返す"""
     response = client.chat.completions.create(model=model, messages=messages, tools=tools)
     response_message = response.choices[0].message
 
     # ツール呼び出しがない場合
     if not hasattr(response_message, "tool_calls"):
-        return (False, [response_message])
+        return (None, [response_message])
     if not response_message.tool_calls:
-        return (False, [response_message])
+        return (None, [response_message])
 
     # ツール呼び出しがある場合
     tool_call = response_message.tool_calls[0]
-    arguments = json.loads(s=tool_call.function.arguments)
-    num1 = arguments.get("num1")
-    num2 = arguments.get("num2")
-    result = add_numbers(num1=num1, num2=num2)
+    if tool_call.function.name == "add_numbers":
+        function_call_result_message = add_numbers(tool_call=tool_call)
+        return (tool_call.function.name, [response_message, function_call_result_message])
 
-    function_call_result_message = {
-        "role": "tool",
-        "content": json.dumps({"num1": num1, "num2": num2, "result": result}),
-        "tool_call_id": tool_call.id,
-    }
+    elif tool_call.function.name == "multiply_numbers":
+        function_call_result_message = multiply_numbers(tool_call=tool_call)
+        return (tool_call.function.name, [response_message, function_call_result_message])
 
-    return (True, [response_message, function_call_result_message])
+    else:
+        return (None, [response_message])
 
 
-def second_query(client: OpenAI, model: str, messages: list[dict[str, Any]]) -> list[dict[str, Any]]:
+def second_query(
+    client: OpenAI,
+    model: str,
+    messages: list[dict[str, Any]],
+) -> list[dict[str, Any]]:
     """計算結果を含む最終応答を生成し、メッセージを返す"""
     response = client.chat.completions.create(model=model, messages=messages)
     response_message = response.choices[0].message
@@ -91,18 +150,19 @@ def main() -> None:
     first_query_messages = [
         system_message,
         # {"role": "user", "content": "こんにちは"},
-        {"role": "user", "content": "123456789+987654321は？"},
+        # {"role": "user", "content": "12+99は？"},
+        {"role": "user", "content": "11×11は？"},
     ]
 
-    is_tool_called, first_response_message = first_query(
+    called_tool_name, first_response_message = first_query(
         client=client,
         model="gpt-4o",
         messages=first_query_messages,
         tools=tools,
     )
 
-    if not is_tool_called:
-        print("ツール呼び出しなし :", first_response_message[0].content)
+    if not called_tool_name:
+        print(f"なし :", first_response_message[0].content)
         return
 
     final_message = second_query(
@@ -111,7 +171,7 @@ def main() -> None:
         messages=first_query_messages + first_response_message,
     )
 
-    print("ツール呼び出しあり :", final_message[0].content)
+    print(f"あり({called_tool_name}) :", final_message[0].content)
     return
 
 
